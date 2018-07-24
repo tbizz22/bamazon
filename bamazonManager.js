0721ecc624894988368352f9dc37018b254e9c8c
tyler bisbee <tyler.bisbee@gmail.com>
2018-07-24 01:19:34 -0400

    working manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,8 +20,14 @@ var connection = mysql.createConnection({
 
 var divider = "\n_____________________________________________________\n"
 
-
-
+// product constructor
+var ProductGrid = function (id, name, dept, stock, price) {
+    this.id = id;
+    this.name = name;
+    this.stock_quantity = stock;
+    this.price = price;
+    this.dept = dept;
+}
 
 
 openConnection();
@@ -59,10 +65,10 @@ function showManagerMenu() {
             lowInventory();
 
         } else if (res.choice === "Add to Inventory") {
-            console.log(res);
+            updateStock();
 
         } else if (res.choice === "Add New Product") {
-            console.log(res);
+            createProduct();
 
         } else {
             console.log("Something went wrong" + divider);
@@ -80,7 +86,7 @@ function greetManager() {
 function showStock() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        
+
         console.log(divider + "Below is a list of our Inventory:\n");
         console.table(res);
     });
@@ -89,9 +95,114 @@ function showStock() {
 
 function lowInventory() {
     var query = connection.query(
-        "SELECT * FROM products WHERE stock_quantity < 5",
-        function(err, res) {
+        "SELECT * FROM products WHERE stock_quantity < 55",
+        function (err, res) {
             console.table(res);
         }
     );
 }
+
+
+function updateStock() {
+    var item;
+    var currStock;
+    inquirer.prompt(
+        [{
+            type: "input",
+            message: "Which product ID would you like to change the stock for?",
+            name: "itemid"
+        }]
+    ).then(function (res) {
+        item = res.itemid
+        var query = connection.query(
+            "SELECT * FROM products WHERE ?", {
+                item_id: item
+            },
+            function (err, res) {
+                if (err) throw err;
+                currStock = res[0].stock_quantity;
+                console.table(res);
+                inquirer.prompt([{
+                    type: "input",
+                    message: "How many many would you like to order?",
+                    name: "count"
+                }]).then(function (res) {
+                    currStock = parseInt(currStock) + parseInt(res.count);
+                    UpdateInventory(item, currStock)
+                })
+            })
+    })
+}
+
+
+
+
+function UpdateInventory(id, aQuant) {
+    // The right way to do this is to get the value and update the total. This prevents very slow race conditions....The lazy way is to just pass in the new value...which is what im doing.
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?", [{
+                stock_quantity: aQuant
+            },
+            {
+                item_id: id
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+        }
+    );
+}
+
+
+
+function createProduct() {    
+    var id;
+    connection.query("SELECT item_id FROM products ORDER BY item_id DESC LIMIT 1",
+        function (err, res) {
+            if (err) throw err;
+            id = parseInt(res[0].item_id) + 1;         
+            
+            inquirer.prompt([{
+                    name: "product",
+                    message: "What is the product you wish to create?"
+                },
+                {
+                    name: "dept",
+                    message: "What department is this product In?"
+                },
+                {
+                    name: "price",
+                    message: "What is the price?"
+                },
+                {
+                    name: "stock",
+                    message: "How many units are you starting with?"
+                }
+
+            ]).then(function (res) {
+                var product = new ProductGrid(id, res.product, res.dept, res.stock, res.price)
+                addProductToDB(product);
+            })
+        })
+}
+
+function addProductToDB(product) {
+    console.log("Inserting a new product...\n");
+    var query = connection.query(
+      "INSERT INTO products SET ?",
+      {
+        item_id: product.id,
+        product_name: product.name,
+        price: product.price,
+        department_name: product.dept,
+        stock_quantity: product.stock_quantity
+      },
+      function(err, res) {
+        console.log(res.affectedRows + " product inserted!\n");
+  
+      }
+    );
+
+    // logs the actual query being run
+    console.log(query.sql);
+  }
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 BAMAZON!
+
+to do - Build generic function to call after process is complete. Will ask if you want to perform another action or exit
+
+
